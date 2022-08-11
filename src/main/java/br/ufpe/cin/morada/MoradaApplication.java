fc9aac6d9c64888550f6155be5df0ec6f6585095
Joao Pedro Henrique <joao.pedro.hsd@gmail.com>
2022-08-10 23:03:59 -0300

    feat: adicionar boilerplate de Spring

---
+++ src/main/java/br/ufpe/cin/morada/MoradaApplication.java
@@ -0,0 +1,13 @@
+package br.ufpe.cin.morada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoradaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoradaApplication.class, args);
+	}
+
+}
