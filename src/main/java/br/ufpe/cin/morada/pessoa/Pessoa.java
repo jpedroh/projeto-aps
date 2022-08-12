db90030654de426183f40fd4fe4f44a4a99e6842
Joao Pedro Henrique <joao.pedro.hsd@gmail.com>
2022-08-12 09:38:16 -0300

    feat: equals e hash code de Pessoa

--- src/main/java/br/ufpe/cin/morada/pessoa/Pessoa.java
+++ src/main/java/br/ufpe/cin/morada/pessoa/Pessoa.java
@@ -3,13 +3,32 @@ package br.ufpe.cin.morada.pessoa;
 import br.ufpe.cin.morada.util.Email;
 
 public class Pessoa {
-
 	private String nome;
 
 	private Email email;
+
+	public Email getEmail() {
+		return email;
+	}
 
 	public String getNome() {
 		return this.nome;
 	}
 
+	@Override
+	public int hashCode() {
+		return email.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pessoa other = (Pessoa) obj;
+		return other.email.equals(email);
+	}
 }
