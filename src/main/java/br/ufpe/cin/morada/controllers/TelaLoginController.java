3e4e145063489fd46fcccbdd850e9f770c50e64c
Joao Pedro Henrique <joao.pedro.hsd@gmail.com>
2022-08-14 14:51:52 -0300

    refactor: alterar nome do controller

--- src/main/java/br/ufpe/cin/morada/controllers/TelaLoginController.java
+++ src/main/java/br/ufpe/cin/morada/controllers/TelaLoginController.java
@@ -3,22 +3,21 @@ package br.ufpe.cin.morada.controllers;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import br.ufpe.cin.morada.controladores.Fachada;
 import br.ufpe.cin.morada.pessoa.Pessoa;
 
 @Controller
-public class TelaLoginUsuarioController {
+public class TelaLoginController {
+  @Autowired
   private Fachada fachada;
-
-  public TelaLoginUsuarioController(Fachada fachada) {
-    this.fachada = fachada;
-  }
 
   @GetMapping("/login")
   public String view() {
@@ -26,8 +25,8 @@ public class TelaLoginUsuarioController {
   }
 
   @PostMapping("/login")
-  public String logarUsuario(@RequestBody String token, HttpServletResponse response, Model model) {
-    Pessoa pessoa = fachada.logar(token.substring("id_token=".length()));
+  public String logarUsuario(@RequestParam(name = "id_token") String token, HttpServletResponse response, Model model) {
+    Pessoa pessoa = fachada.logar(token);
     response.addCookie(new Cookie("email", pessoa.getEmail().getValor()));
     return "redirect:/";
   }
