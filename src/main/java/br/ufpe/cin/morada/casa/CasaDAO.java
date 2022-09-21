0fc783486ccd2f1de363ea9aa525088951df8e7c
Joao Pedro Henrique <joao.pedro.hsd@gmail.com>
2022-09-20 21:41:37 -0300

    refactor: remover arquivos nao utilizados

--- src/main/java/br/ufpe/cin/morada/casa/CasaDAO.java
+++
@@ -1,7 +0,0 @@
-package br.ufpe.cin.morada.casa;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface CasaDAO extends CrudRepository<Casa, String> {
-
-}
--- src/main/java/br/ufpe/cin/morada/subsistemaGoogle/ISubsistemaGoogle.java
+++
@@ -1,9 +0,0 @@
-package br.ufpe.cin.morada.subsistemaGoogle;
-
-import br.ufpe.cin.morada.pessoa.Pessoa;
-
-public interface ISubsistemaGoogle {
-
-	public Pessoa getDadosFromToken(String token);
-
-}
