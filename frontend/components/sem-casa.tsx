44dfc43a2bec6791b436d7d2b0b53b92f5fdcff4
Joao Pedro Henrique <joao.pedro.hsd@gmail.com>
2022-09-20 21:39:40 -0300

    refactor: alterar funcao de submissão

--- frontend/components/sem-casa.tsx
+++ frontend/components/sem-casa.tsx
@@ -6,9 +6,11 @@ export function SemCasa() {
   const criarCasa = useCriarCasa();
   const entrarCasa = useEntrarCasa();
 
-  function handleSubmit<T>(evt: FormEvent<HTMLFormElement>) {
-    evt.preventDefault()
-    return Object.fromEntries(new FormData(evt.target as any)) as T
+  function handleSubmit<T>(action: (x: T) => void) {
+    return function (evt: FormEvent<HTMLFormElement>) {
+      evt.preventDefault()
+      return action(Object.fromEntries(new FormData(evt.target as any)) as T)
+    }
   }
 
   return <div className='flex flex-col items-center'>
@@ -16,14 +18,14 @@ export function SemCasa() {
     <div className='flex gap-6'>
       <div className='flex flex-col gap-4 p-6 rounded-xl'>
         <h3 className='text-xl text-center font-semibold'>Criar uma casa</h3>
-        <form className='flex flex-col gap-2' onSubmit={evt => criarCasa.mutate(handleSubmit(evt))}>
+        <form className='flex flex-col gap-2' onSubmit={handleSubmit(criarCasa.mutate)}>
           <input className='border border-black p-2 rounded' placeholder='Nome da casa' name="nome" required />
           <button disabled={criarCasa.isLoading} className='bg-blue-400 p-2 rounded hover:bg-blue-300'>Criar casa</button>
         </form>
       </div>
       <div className='flex flex-col gap-4 p-6 rounded-xl'>
         <h3 className='text-xl text-center font-semibold'>Entrar numa casa existente</h3>
-        <form className='flex flex-col gap-2' onSubmit={evt => entrarCasa.mutate(handleSubmit(evt))}>
+        <form className='flex flex-col gap-2' onSubmit={handleSubmit(entrarCasa.mutate)}>
           <input className='border border-black p-2 rounded' placeholder='Código da casa' name="codigo" required />
           <button className='bg-blue-400 p-2 rounded hover:bg-blue-300'>Entrar na casa</button>
         </form>
